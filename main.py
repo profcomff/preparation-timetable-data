f334fd7d62a4267b0b0ca133f18c40377fdf77af
Daiwery <71455347+Daiwery@users.noreply.github.com>
2022-08-24 17:16:06 +0300

    Parsing (#2)
    * Parse group
    
    * Parse place
    
    * Fix
    
    * Parse subjects

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from pipelines.parse_name import parse_name
-from pipelines.parse_teacher import parse_teacher
-from pipelines.parse_timetable import parse_timetable
 import pandas as pd
 
-# lessons = parse_timetable()
-lessons = pd.read_excel("parsed_lessons_table.xlsx", sheet_name=0)
-# lessons = parse_name(lessons)
+from pipelines.parse_group import parse_group
+from pipelines.parse_place import parse_place
+from pipelines.parse_subjects import parse_subjects
+from pipelines.parse_teacher import parse_teacher
 
+# lessons = parse_timetable()
+# lessons = parse_name(lessons)
+lessons = pd.read_excel("parsed_lessons_table.xlsx", sheet_name=0)
+lessons = lessons.drop(columns=["id"])
+
+lessons, places = parse_place(lessons)
+lessons, groups = parse_group(lessons)
+lessons, subjects = parse_subjects(lessons)
 lessons, teachers = parse_teacher(lessons)
-print(lessons)
-print(teachers)
+
+with open("lessons.xlsx", "wb") as f:
+    writer = pd.ExcelWriter(f, engine="xlsxwriter")
+    lessons.to_excel(writer, index=False)
+    writer.save()
---
+++ pipelines/parse_subjects.py
@@ -0,0 +1,122 @@
+import re
+
+import pandas as pd
+
+
+def parse_subjects(lessons):
+    """
+    Парсит колонку 'subject' и, если надо, удаляет строчку из таблицы (если в названии предметы указаны группы).
+    Дополнительно возвращает список предметов.
+    """
+    subjects = []
+    deleted_rows = []
+    for index, row in lessons.iterrows():
+        subject = row["subject"]
+
+        # 307 - S
+        result = re.match("(\d+[А-Яа-яёЁ]*) - ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] != result[1]:
+                    deleted_rows.append(index)
+                else:
+                    subjects.append(result[2])
+                continue
+
+        # 307, 302 - S
+        result = re.match("(\d+[А-Яа-яёЁ]*)[, +]*(\d+[А-Яа-яёЁ]*) - ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] != result[1] and row["group"] != result[2]:
+                    deleted_rows.append(index)
+                else:
+                    subjects.append(result[3])
+                continue
+
+        # 307, 302, 306 - S
+        result = re.match("(\d+[А-Яа-яёЁ]*)[, +]*(\d+[А-Яа-яёЁ]*)[, +]*(\d+[А-Яа-яёЁ]*) - ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] != result[1] and row["group"] != result[2] and row["group"] != result[3]:
+                    deleted_rows.append(index)
+                else:
+                    subjects.append(result[4])
+                continue
+
+        # 307 - S, 302 - S
+        result = re.match("(\d+[А-Яа-яёЁ]*) - ([А-Яа-яёЁ \./]+), (\d+[А-Яа-яёЁ]*) - ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] == result[1]:
+                    subjects.append(result[2])
+                elif row["group"] == result[3]:
+                    subjects.append(result[4])
+                else:
+                    deleted_rows.append(index)
+                continue
+
+        # 307 S, 302 S
+        result = re.match("(\d+[А-Яа-яёЁ]*) ([А-Яа-яёЁ \./]+), (\d+[А-Яа-яёЁ]*) ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] == result[1]:
+                    subjects.append(result[2])
+                elif row["group"] == result[3]:
+                    subjects.append(result[4])
+                else:
+                    deleted_rows.append(index)
+                continue
+
+        # 1 поток без 307 группы - S
+        result = re.match("1 поток без 307 группы - ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] == "307":
+                    deleted_rows.append(index)
+                else:
+                    subjects.append(result[1])
+                continue
+
+        # 1 поток без 307 группы и астр. - S
+        result = re.match("1 поток без 307 группы и астр. - ([А-Яа-яёЁA-Z \./]+)", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] == "307" or row["group"] == "301":
+                    deleted_rows.append(index)
+                else:
+                    subjects.append(result[1])
+                continue
+
+        # 306
+        result = re.match("(\d+[А-Яа-яёЁ]*) *", subject)
+        if not (result is None):
+            if subject == result[0]:
+                if row["group"] != result[1]:
+                    deleted_rows.append(index)
+                else:
+                    subjects.append(result[1])
+                continue
+
+        subjects.append(subject)
+
+    lessons.drop(deleted_rows, axis=0, inplace=True)
+    lessons["subject"] = subjects
+
+    teachers = []
+    subjects = []
+    for index, row in lessons.iterrows():
+        if "Специальный физический практикум" in row["subject"]:
+            result = "".join(re.findall("[А-Яа-яёЁ]+ [А-Яа-яёЁ]{1}\. [А-Яа-яёЁ]{1}\.", row["subject"]))
+            if result:
+                subjects.append("Специальный физический практикум" )
+                teachers.append("".join(re.findall("[А-Яа-яёЁ]+ [А-Яа-яёЁ]{1}\. [А-Яа-яёЁ]{1}\.", row["subject"])))
+                continue
+
+        teachers.append(row["teacher"])
+        subjects.append(row["subject"])
+
+
+    lessons["teacher"] = teachers
+    lessons["subject"] = subjects
+
+    return lessons, list(set(lessons["subject"].tolist()))
