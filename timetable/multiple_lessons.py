31814246aec64b392660fa55cc1a96dac015e6f4
astrovlad <102761503+astrovlad@users.noreply.github.com>
2022-11-14 20:07:45 +0300

    Update multiple_lessons.py

--- timetable/multiple_lessons.py
+++ timetable/multiple_lessons.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def _multiple_lessons(df):
+def multiple_lessons(df):
     for (weekday, group, subject, start), sub_df in df.groupby(['weekday', 'group', 'subject', 'start']):
         if len(sub_df) > 1:
             teachers = sub_df['teacher'].values
