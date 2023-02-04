f6cd7bfa048d5eb03b386910679c6cad34478b0a
SergeyZamyatin1 <sergey.zamyatin.01@list.ru>
2023-02-04 11:13:55 +0300

    Fix add_lessons

--- database/add_lessons.py
+++ database/add_lessons.py
@@ -1,10 +1,18 @@
 import logging
 
 import requests
+from requests.exceptions import RequestException
+from retrying import retry
 
 from utilities import urls_api
 
 _logger = logging.getLogger(__name__)
+
+
+@retry(retry_on_exception=lambda e: isinstance(e, RequestException), wait_exponential_multiplier=1000,
+       wait_exponential_max=30000, stop_max_attempt_number=30)
+def upload_event(event, headers, base):
+    requests.post(urls_api.get_url_event(urls_api.MODES_URL.post, base), json=event, headers=headers)
 
 
 def add_lessons(lessons, headers, base):
@@ -13,13 +21,14 @@ def add_lessons(lessons, headers, base):
     """
     _logger.info("Загружаю пары в базу данных...")
 
-    url_post_event = urls_api.get_url_event(urls_api.MODES_URL.post, base) + 'bulk'
-    lessons.rename(
-        columns={'group': 'group_id', 'subject': 'name', 'place': 'room_id', 'start': 'start_ts', 'end': 'end_ts',
-                 'teacher': 'lecturer_id'}, inplace=True)
-    lessons.pop('index')
+    for i, row in lessons.iterrows():
+        event = {
+            "name": row['subject'],
+            "room_id": row['place'],
+            "group_id": int(row['group']),
+            "lecturer_id": row['teacher'],
+            "start_ts": row['start'],
+            "end_ts": row['end']
+        }
+        upload_event(event, headers, base)
 
-    result = lessons.to_json(orient='records', force_ascii=False).encode('utf-8')
-
-    r = requests.post(url=url_post_event, data=result, headers=headers)
-    _logger.debug(r)
