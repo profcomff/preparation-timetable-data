af2750d8f31dac7bb75b035508724df01890a48e
Daiwery <andrey.luk10@gmail.com>
2023-02-12 10:26:51 +0300

    Refactoring

--- timetable/__init__.py
+++ timetable/__init__.py
@@ -1,4 +1,4 @@
-from timetable.classical_parse_timetable import classical_parse_timetable
+from timetable.semester_parse_timetable import classical_parse_timetable
 from timetable.parse_all import parse_all
 from timetable.calc_date import calc_date
 
